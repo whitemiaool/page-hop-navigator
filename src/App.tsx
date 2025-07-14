952e8883720e8b0e3aaaf98e53d5710bb81a7439
dyxuan <duyingxuan.dyx@antgroup.com>
2025-07-14 14:06:50 +0800

    feat: 增加多元素选择功能，更新聊天组件以支持多选元素的处理

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index2222";
-import About from "./pages/About";
+import About from "./pages/About22222";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
